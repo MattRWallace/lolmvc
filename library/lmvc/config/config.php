faf8b85ab9940054242db6e89f2ca134bc6c1f4c
David Mitchel <mitzip@cs.txstate.edu>
2012-06-22 12:12:20 -0500

    structure cleanup

--- library/lmvc/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
-
-define('MONGO_SERVER', 'mongodb://localhost:27017');
-define('MONGO_DATABASE', 'punymvc_demo');
--- library/lmvc/views/posts/index.php
+++
@@ -1,5 +0,0 @@
-Here are the posts:<br /><br />
-<?php foreach ($vars['posts'] as $post) : ?>
-	<?php $post_uri = sprintf('%s/posts/view/%u', dirname($_SERVER['PHP_SELF']), $post['_id']) ?>
-	<a href="<?php echo $post_uri ?>"><?php echo $post['title'] ?></a> by <?php echo $post['author'] ?><br />
-<?php endforeach ?>
